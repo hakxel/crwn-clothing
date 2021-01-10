23a4da183dc0b783fc656820a26dbe392b7c74ca
Hakxel <asiel_montes@yahoo.com>
2021-01-09 22:52:47 -0500

    Finish sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -15,12 +15,21 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
-    event.PreventDefault();
-    this.setState({
-      email: '',
-      password: ''
-    })
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: '',
+        password: ''
+      })
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 
   handleChange = e => {
