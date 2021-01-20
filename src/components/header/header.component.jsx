fd76f9312ba8c8456a1828a1c768b1c26ed5482a
Hakxel <asiel_montes@yahoo.com>
2021-01-19 22:23:00 -0500

    Add cart icon and cart dropdown components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 const Header = ({currentUser}) => (
   <div className="header">
@@ -23,7 +25,9 @@ const Header = ({currentUser}) => (
         :
         <Link className='option' to='/signin'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 )
 
