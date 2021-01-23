37297d0f9fa6f3b7ee13e76f0ca2d9ebbafcf2ec
Hakxel <asiel_montes@yahoo.com>
2021-01-23 10:32:59 -0500

    Add checkout page, checkout item, and update cart to use selectors

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,7 +3,7 @@ import './cart-item.styles.scss';
 
 const CartItem = ({item: { imageUrl, price, name, quantity }}) => (
   <div className="cart-item">
-    <img src={imageUrl}/>
+    <img src={imageUrl} alt="cart item"/>
     <div className="item-details">
       <span className="name">{name}</span>
       <span className="price">{quantity} x ${price}</span>
