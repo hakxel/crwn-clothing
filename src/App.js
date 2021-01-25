4149c62d74491905d111479f556e125b505eae44
Hakxel <asiel_montes@yahoo.com>
2021-01-25 07:05:35 -0500

    Get shop data from firestore into redux and add spinner as a HOC

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ import Header from './components/header/header.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions'; 
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 import { createStructuredSelector } from 'reselect';
 
 
@@ -21,7 +22,7 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const {setCurrentUser /*, collectionsArray */} = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -34,6 +35,9 @@ class App extends Component {
           })
         });
 
+        // addCollectionAndDocuments('collections', collectionsArray.map(
+        //   ({title, items}) => ({title, items})
+        // ));
       } else {
         setCurrentUser(userAuth);
       }
@@ -61,7 +65,8 @@ class App extends Component {
 }
 
 const mapStateToProps =createStructuredSelector ({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  /* collectionsArray: selectCollectionsForPreview */
 })
 
 const mapDispatchToProps = dispatch => ({
